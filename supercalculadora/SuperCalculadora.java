a78240c58ad32b31444cd9a6480a74e62bd65e2a
BrenoPinh <201816252+BrenoPinh@users.noreply.github.com>
2025-05-26 15:06:46 -0300

    .

---
+++ supercalculadora/SuperCalculadora.java
@@ -0,0 +1,21 @@
+package supercalculadora;
+
+import java.util.Scanner;
+
+public class SuperCalculadora {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o número que deseja obter os cálculos:");
+        double numero = (double) teclado.nextDouble();
+        double restDiv = (double) numero%2;
+        double cub = (double) Math.pow(numero,3);
+        double rQuad = (double) Math.sqrt(numero);
+        double rCub = (double) Math.cbrt(numero);
+        double absoluto = (double) Math.abs(numero);
+        System.out.println("Resto da Divisão por 2: " + restDiv);
+        System.out.println("Elevado ao Cubo: " + cub);
+        System.out.println("Raiz Quadrada: " + rQuad);
+        System.out.println("Raiz cúbica: " + rCub) ;
+        System.out.println("Valor absoluto: " + absoluto);
+    }
+}
