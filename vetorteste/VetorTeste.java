4e957b09a5b3a211eac2fa40fbb979d14828c085
BrenoPinh <201816252+BrenoPinh@users.noreply.github.com>
2025-06-04 16:18:21 -0300

    Create VetorTeste.java

---
+++ vetorteste/VetorTeste.java
@@ -0,0 +1,12 @@
+package vetorteste;
+
+public class VetorTeste {
+    public static void main(String[] args) {
+        int num[] = {3, 2, 5, 1, 4, 10, 11, 5, 4, 22};
+        System.out.println(num[2]);
+
+        for (int c = 0 ; c < num.length; c++){
+            System.out.println(num[c]);
+        };
+    }
+}
